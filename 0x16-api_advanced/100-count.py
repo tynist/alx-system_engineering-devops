5edf87b05117233ebe14839223daf3fabfa1b851
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-02-27 23:42:12 +0100

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -4,13 +4,8 @@ import requests
 
 
 def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """Prints counts of given words found in hot posts of a given subreddit.
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
+    """
+    Prints counts of given words found in hot posts of a given subreddit
     """
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
